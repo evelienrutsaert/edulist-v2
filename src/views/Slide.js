32ed14eeb018d1d0e7f50eb626f375a4a1c214f2
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2024-03-08 09:28:20 +0100

    changed url slides

--- src/views/Slide.js
+++ src/views/Slide.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ModalPopUp from "../components/ModalPopUp";
 import { useQuery } from "@apollo/client";
 import { SLIDE } from "../graphql/queries";
@@ -7,7 +7,19 @@ export default function Slide({ id, openModal, setOpenModal }) {
 	const { loading, error, data } = useQuery(SLIDE, {
 		variables: { id: id },
 	});
+	const [src, setSrc] = useState("");
 
+	useEffect(() => {
+		if (data) {
+			if (data.slide.slideX == 0 && data.slide.slideY == 0) {
+				setSrc(`${data.slide.slideUrl}`);
+			} else {
+				setSrc(
+					`${data.slide.slideUrl}#/${data.slide.slideX}/${data.slide.slideY}`
+				);
+			}
+		}
+	}, [data]);
 	return (
 		<>
 			{loading && <p>Loading data ....</p>}
@@ -15,7 +27,7 @@ export default function Slide({ id, openModal, setOpenModal }) {
 			{data && (
 				<ModalPopUp
 					title={data.slide.title}
-					src={`${data.slide.slideUrl}#/${data.slide.slideX}/${data.slide.slideY}`}
+					src={src}
 					openModal={openModal}
 					setOpenModal={setOpenModal}
 				/>
